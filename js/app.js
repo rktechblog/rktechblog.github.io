5029817ce12fcc7cd76b2cd861f8b011eeabd7ad
rajkumar.selvam <raajkumar.jj@gmail.com>
2016-06-24 15:20:54 +0530

    Updated about.html page

--- js/app.js
+++ js/app.js
@@ -1,21 +1,30 @@
-var mApp = angular.module("mApp", []);
+var mApp = angular.module("mApp", [])
+    .controller("ArticleController", function ($scope, $http) {
+        $http.get('./data/main-articles.json').
+        success(function (data) {
+            $scope.articles = data.articles;
+        }).
+        error(function (error) {
+            console.log("Error reading main-articles.json: " + error);
+        });
+    })
 
-mApp.controller("ArticleController", function ($scope, $http) {
-    $http.get('./data/main-articles.json').
-    success(function (data) {
-        $scope.articles = data.articles;
-    }).
-    error(function (error) {
-        console.log("Error reading main-articles.json: " + error);
-    });
-});
+    .controller("ProfilesController", function ($scope, $http) {
+        $http.get('./data/profiles.json').
+        success(function (data) {
+            $scope.profiles = data.profiles;
+        }).
+        error(function (error) {
+            console.log("Error reading profiles.json: " + error);
+        });
+    })
 
-mApp.controller("ProfilesController", function ($scope, $http) {
-    $http.get('./data/profiles.json').
-    success(function (data) {
-        $scope.profiles = data.profiles;
-    }).
-    error(function (error) {
-        console.log("Error reading profiles.json: " + error);
+    .controller("AboutController", function ($scope, $http) {
+        /*$http.get('./data/profiles.json').
+        success(function (data) {
+            $scope.profiles = data.profiles;
+        }).
+        error(function (error) {
+            console.log("Error reading profiles.json: " + error);
+        });*/
     });
-});
